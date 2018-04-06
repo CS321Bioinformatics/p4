740eda8d0df8cb16f419efe49f51882970d2b732
Jason Smith <jasonsmith7@u.boisestate.edu>
2018-04-06 13:45:42 -0600

    created driver classes

---
+++ src/GeneBankCreateBTree.java
@@ -0,0 +1,5 @@
+package src;
+
+public class GeneBankCreateBTree{
+  //TODO Driver class to create a btree file from a given gbk file
+}
---
+++ src/GeneBankSearch.java
@@ -0,0 +1,5 @@
+package src;
+
+public class GeneBankSearch{
+  //TODO driver class to perform search on a user provided btree file
+}
