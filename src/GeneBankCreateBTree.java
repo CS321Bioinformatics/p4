2ad179492b5cf2920f14e0ae23cc4fc61da31fa0
Jason Smith <jasonsmith7@u.boisestate.edu>
2018-04-12 17:51:56 -0600

    added main method

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -2,4 +2,8 @@ package src;
 
 public class GeneBankCreateBTree{
   //TODO Driver class to create a BTree file from a given gbk file
+	
+	public static void main(string[] args) {
+		
+	}
 }
--- src/GeneBankSearch.java
+++ src/GeneBankSearch.java
@@ -2,4 +2,7 @@ package src;
 
 public class GeneBankSearch{
   //TODO driver class to perform search on a user provided BTree file
+public static void main(string[] args) {
+		
+	}
 }
