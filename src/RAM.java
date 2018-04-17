e858ec3bbdaab9c7e9eb19f7395d23e57707b503
ryleystuder <ryepodstuder@gmail.com>
2018-04-17 01:14:53 -0600

    Added some getters and a second constructor in TreeObject
    Created methods to convert file subsequence to parsable values in RAM class

--- src/RAM.java
+++ src/RAM.java
@@ -2,5 +2,42 @@ package src;
 
 public class RAM {
 
-    //TODO read/write from files
+    //TODO Converts DNA substrings for GeneBankCreate driver
+
+    public long getKey() {
+        return key;
+    }
+
+    private long key = 0;
+
+
+
+
+    public long convertGBKtoSubseq(String input)
+    {
+        input = input.toUpperCase();
+        long key = 0x00000000;
+        for(int i = 0; i<input.length(); i++){
+            //convert String char to 0,1,2,3
+            //switch statements
+
+            //key |= position(position = tbits << 2i)
+        }
+
+
+
+        return key;
+
+    }
+
+
+    public long convertLongtoString(Long input, int length)
+    {
+        String convert;
+        long temp;
+
+        return input;
+
+    }
+
 }
--- src/TreeObject.java
+++ src/TreeObject.java
@@ -6,16 +6,32 @@ public class TreeObject {
 	//Metadata
 	//long - dna substring - data - comparable by key
 	//int frequency
-	
-	//
-	
+
+
 	//Linked List or ArrayList of tree objects
 	private long dnaString;
 	private int frequency;
-	
-	
-	public TreeObject(long dna){
-		frequency = 0;
-		dnaString = dna;
+
+
+	public long getDnaString() {
+		return dnaString;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void incrementFrequency() {
+		this.frequency++;
+	}
+
+	//Constructors
+	public TreeObject(long dnaString){
+		this.frequency = 0;
+		this.dnaString = dnaString;
+	}
+	public TreeObject(long dnaString, int frequency){
+		this.frequency = frequency;
+		this.dnaString = dnaString;
 	}
 }
