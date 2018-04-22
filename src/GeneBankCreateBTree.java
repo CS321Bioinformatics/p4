7fc995f5b4ff57fc4e9014ebf2bed1b37e7c9665
Jason Smith <jasonsmith7@u.boisestate.edu>
2018-04-21 23:32:15 -0600

    test

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class GeneBankCreateBTree {
-  //TODO Driver class to create a BTree file from a given gbk file	
+	//TODO Driver class to create a BTree file from a given gbk file	
 	//args format: <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]
 	public static void main(String[] args) {
 		int cacheFlag, degree, seqLength, cacheSize, debugLevel;
@@ -17,9 +17,9 @@ public class GeneBankCreateBTree {
 		StringBuilder totalDNA = new StringBuilder();
 		char token;
 		boolean readLine = false ;
-		
+
 		if ((args.length == 0) || (args.length < 4 || args.length > 6)) { //verify correct amount of args
-			printUsage();
+			//printUsage();
 			System.exit(1);
 		}else {
 			try {
@@ -27,43 +27,43 @@ public class GeneBankCreateBTree {
 					cacheSize = Integer.parseInt(args[4]);
 					debugLevel = Integer.parseInt(args[5]);
 				}
-				
+
 				cacheFlag = Integer.parseInt(args[0]);
 				degree = Integer.parseInt(args[1]);
 				seqLength = Integer.parseInt(args[3]);
 				gbk = new File(args[2]);
-				
-//				BTreeNode tree = new BTreeNode();
-				
+
+				//				BTreeNode tree = new BTreeNode();
+
 				//Parse File
 				lineScan = new Scanner(gbk);
 				while(lineScan.hasNextLine()) {
 					line = lineScan.nextLine();
-					
+
 					if(line.substring(0,5).equals("ORIGIN")) {
 						readLine = true;
 					}else if(line.substring(0,5).equals("//")) {
 						readLine = false;
 					}
-					if(readline) {
+					if(readLine) {
 						charScan = new Scanner(line);
 						while (charScan.hasNext()) {
-							token = charScan.Next();
-							if (token != '\n' || !token.isDigit() || token != ' ') {
+							token = charScan.next();
+							if (token != '\n' || token != ' ') {
 								totalDNA.append(token);
 							}
 						}
 						charScan.close();
 					}
-					
+
 				}
 				lineScan.close(); 
-				
+
 			} catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } finally {
-            	DNA = totalDNA.toString();
-            	int index = 0;
+				e.printStackTrace();
+			} finally {
+				DNA = totalDNA.toString();
+				int index = 0;
 				while((index + seqLength) < DNA.length()) {
 					String currString = DNA.substring(index,index + seqLength);
 					boolean isValid = true;
@@ -73,29 +73,28 @@ public class GeneBankCreateBTree {
 						}
 					}
 					if (isValid) {
-							//convert currString to long
-//								long sequence = convertGBKtoSubseq(currString);
-							//make tree object
-//								TreeObject newObj = new TreeObject(sequence);
-							//search for object
-								//if found increment freq
-								//else insert
-									//either into and existing node or a new node
-							
-								
+						//convert currString to long
+						//								long sequence = convertGBKtoSubseq(currString);
+						//make tree object
+						//								TreeObject newObj = new TreeObject(sequence);
+						//search for object
+						//if found increment freq
+						//else insert
+						//either into and existing node or a new node
+
+
 					}
 				}
 				//output dump or poop
-				
-            }
-            }
+
+			}
+		}
 		System.out.println(DNA);
-		return 0;
-        }
-		
 	}
-	
+
+}
+/*
 	public void printUsage() {
 		System.out.println("Expected Format for arguments: <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
-	}
-}
+
+	}*/
