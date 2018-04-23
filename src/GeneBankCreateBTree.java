306ddbe5df3062640f8e89f7577594a4b8e4cad8
Jason Smith <jasonsmith7@u.boisestate.edu>
2018-04-23 10:25:09 -0600

    got that parsing working finally, right now it just prints all sequences of the command arg sequence length and then "done"

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,4 +1,4 @@
-package src;
+//package src;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -9,13 +9,13 @@ public class GeneBankCreateBTree {
 	//TODO Driver class to create a BTree file from a given gbk file	
 	//args format: <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]
 	public static void main(String[] args) {
-		int cacheFlag, degree, seqLength, cacheSize, debugLevel;
+		int cacheFlag, degree, seqLength = 0, cacheSize, debugLevel;
 		String gbkFileName;
 		File gbk;
 		Scanner lineScan, charScan;
-		String line, DNA;
+		String line = "", DNA = "";
 		StringBuilder totalDNA = new StringBuilder();
-		char token;
+		String token;
 		boolean readLine = false ;
 
 		if ((args.length == 0) || (args.length < 4 || args.length > 6)) { //verify correct amount of args
@@ -40,17 +40,22 @@ public class GeneBankCreateBTree {
 				while(lineScan.hasNextLine()) {
 					line = lineScan.nextLine();
 
-					if(line.substring(0,5).equals("ORIGIN")) {
-						readLine = true;
-					}else if(line.substring(0,5).equals("//")) {
+					
+					if(line.substring(0,2) == ("//")) {
 						readLine = false;
+						break;
+					}else if(line.substring(0,6).equals("ORIGIN")) {
+						readLine = true;
+						line = lineScan.nextLine();
 					}
 					if(readLine) {
 						charScan = new Scanner(line);
 						while (charScan.hasNext()) {
 							token = charScan.next();
-							if (token != '\n' || token != ' ') {
-								totalDNA.append(token);
+							for(int x = 0; x < token.length();++x){
+							  if ((token.charAt(x) != ('\n')) && (token.charAt(x) != ' ') && (token.charAt(x) < 48  ||  token.charAt(x) > 57) ) {
+								  totalDNA.append(token.charAt(x));
+							  }
 							}
 						}
 						charScan.close();
@@ -81,15 +86,17 @@ public class GeneBankCreateBTree {
 						//if found increment freq
 						//else insert
 						//either into and existing node or a new node
-
-
+					System.out.println(currString);	
 					}
+					index++;
 				}
 				//output dump or poop
-
+				System.out.println("done");
+				System.exit(0);
 			}
+			
 		}
-		System.out.println(DNA);
+		
 	}
 
 }
